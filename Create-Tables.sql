dbf3a9813906dfb46abc07d47f020f73469129e1
taq95266-arch <taq95266@gmail.com>
2026-09-23 12:16:06 +0400

    Add files via upload
    update create table

--- Create-Tables.sql
+++ Create-Tables.sql
@@ -3,9 +3,9 @@ USE Airline
 
 CREATE TABLE airport(
 Airport_code CHAR(4) PRIMARY KEY,
-Name VARCHAR(10),
-City VARCHAR(10),
-Country VARCHAR(10),
+Name VARCHAR(10) NOT NULL,
+City VARCHAR(10) NOT NULL,
+Country VARCHAR(10) NOT NULL,
 );
 
 
@@ -15,23 +15,27 @@ SELECT*FROM airport
 CREATE TABLE aircraft(
 Registration_num CHAR(4) PRIMARY KEY,
 Num_of_manufactor INT,
-Model VARCHAR(10),
-Manufactor VARCHAR(10),
+Model VARCHAR(10) NOT NULL,
+Manufactor VARCHAR(10) NOT NULL,
 Year_of_manufactor INT,
-Seating_capacity INT,
+Seating_capacity INT NOT NULL,
+CONSTRAINT ck_aircraft_Capacity CHECK ( Seating_capacity >0),
 );
 
 SELECT*FROM aircraft
 
 CREATE TABLE Flight(
 Flight_ID CHAR(4) PRIMARY KEY,
-Status VARCHAR(10),
-Departmrnt_datetime VARCHAR(10),
-Arrival_datetime VARCHAR(10),
+Status VARCHAR(10) NOT NULL DEFAULT' Scheduled',
+Departmrnt_datetime VARCHAR(10) NOT NULL  ,
+Arrival_datetime VARCHAR(10) NOT NULL ,
 Airport_code CHAR(4),
 Registration_num CHAR(4),
 FOREIGN KEY (Airport_code) REFERENCES airport(Airport_code),
-FOREIGN KEY (Registration_num ) REFERENCES aircraft(Registration_num )
+FOREIGN KEY (Registration_num ) REFERENCES aircraft(Registration_num ),
+
+ CONSTRAINT CK_fLight_status CHECK(Status IN ('Scheduled', 'Delayed', 'Cancelled', 'Completed')),
+ CONSTRAINT ST_fLight_TIME CHECK ( Arrival_datetime >Departmrnt_datetime),
 );
 
 DROP TABLE Flight
@@ -42,26 +46,29 @@ SELECT*FROM Flight
 
 CREATE TABLE Passenger(
 National_ID CHAR(4) PRIMARY KEY,
-Full_Name VARCHAR(10),
+Full_Name VARCHAR(10) NOT NULL,
 DOB DATE ,
-E_mail VARCHAR(10),
-Phone VARCHAR(10),
-Nationally VARCHAR(10),
+E_mail VARCHAR(10) NOT NULL,
+Phone VARCHAR(10) NOT NULL ,
+Nationally VARCHAR(10) NOT NULL,
 );
 
 SELECT*FROM Passenger
 
 CREATE TABLE Booking(
 Booking_ID CHAR(4) PRIMARY KEY,
-Seat_num VARCHAR(10),
-City VARCHAR(10),
+Seat_num VARCHAR (10),
+City VARCHAR(10) NOT NULL,
 Class VARCHAR(10),
-Booking_Date DATE ,
+Booking_Date DATE  DEFAULT'CURRENT DATE',
 Price Decimal,
-National_ID CHAR(4),
-Flight_ID CHAR(4),
+National_ID CHAR(4) NOT NULL,
+Flight_ID CHAR(4) NOT NULL,
 FOREIGN KEY (National_ID) REFERENCES Passenger(National_ID),
 FOREIGN KEY (Flight_ID) REFERENCES Flight (Flight_ID),
+CONSTRAINT CK_Booking_class CHECK(Class IN ('Economy', 'Business', 'First')),
+CONSTRAINT ck_Booking_Price CHECK ( Price >0),
+ 
 );
 SELECT*FROM Booking
 
@@ -69,10 +76,11 @@ DROP TABLE Booking
 
 CREATE TABLE Crew_member(
 Crew_ID CHAR(4) PRIMARY KEY,
-Full_Name VARCHAR(10),
-Role VARCHAR(10),
+Full_Name VARCHAR(10) NOT NULL,
+Role VARCHAR(10) NOT NULL,
 Flight_ID CHAR(4),
 FOREIGN KEY (Flight_ID) REFERENCES Flight (Flight_ID),
+CONSTRAINT CK_Crew_member CHECK(Role IN ('Pilot', 'Co-Pilot', 'Flight Attendant', 'Engineer')),
 );
 SELECT*FROM Crew_member
 
